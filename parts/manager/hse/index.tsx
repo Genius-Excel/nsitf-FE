167b98e086a5cee7e70d98db6ab9f52cdfcae50e
hastrotech1 <hastrotech1.com>
2025-11-20 21:51:20 +0100

    fixed import error

--- parts/manager/hse/index.tsx
+++ parts/manager/hse/index.tsx
@@ -1,11 +1,11 @@
 "use client"
-import HSEManagement from "../../admin/hse/hseFunc"
+import { HSEDashboardContent } from "../../admin/hse/hseFunc"
 
 
 export default function HSEPage() {
   return (
     <div className="container mx-auto p-4">
-      <HSEManagement />
+      <HSEDashboardContent />
     </div>
   )
 }
