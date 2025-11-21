6e25e92c5b08903a5d197f249ea9be270f3f9702
hastrotech1 <hastrotech1.com>
2025-11-21 10:57:00 +0100

    controlled build error

--- parts/manager/hse/index.tsx
+++ parts/manager/hse/index.tsx
@@ -1,11 +1,10 @@
-"use client"
-import { HSEDashboardContent } from "../../admin/hse/hseFunc"
-
+"use client";
+import HSEDashboardContent from "../../admin/hse/hseFunc";
 
 export default function HSEPage() {
   return (
     <div className="container mx-auto p-4">
       <HSEDashboardContent />
     </div>
-  )
+  );
 }
